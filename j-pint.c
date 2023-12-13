1bdf9881071941af66380ec200655fe4b2eed44d
KingJ89 <janmutewera@gmail.com>
2023-12-13 07:59:25 -0800

    j-pint and j-pop files created and tested successfully

---
+++ j-pint.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * pint - Prints value at the top of stack.
+ * @stack: Double pointer to the top of stack.
+ * @line_number: Line number in monty byte code.
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't j-pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
