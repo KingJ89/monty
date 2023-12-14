17d5f3f1d480d02643d675d938725188065c0a67
KingJ89 <janmutewera@gmail.com>
2023-12-14 11:44:28 -0800

    swap file added and betty tested

---
+++ j-swap.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * swap - swaps top two of the stack.
+ * @stack: Double pointer.
+ * @line_number: number in the monty byte code file
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%uU: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+
+	}
+
+	temp = *stack;
+
+	*stack = (*stack)->next;
+
+	if (*stack != NULL)
+		(*stack)->prev =  NULL;
+
+	free(temp);
+}
